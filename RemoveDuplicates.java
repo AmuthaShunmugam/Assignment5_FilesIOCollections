3a35fc658fa3e90c07eb6650bd5f5008128dce1f
AmuthaShunmugam <45200546+AmuthaShunmugam@users.noreply.github.com>
2018-11-25 13:24:31 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+class RemoveDuplicates 
+{ 
+void removeDuplicates(String str) 
+{ 
+LinkedHashSet<Character> lhs = new LinkedHashSet<>(); 
+for(int i=0;i<str.length();i++) 
+lhs.add(str.charAt(i)); 
+for(Character ch : lhs) 
+System.out.print(ch); 
+} 
+public static void main(String args[]) 
+{ 
+String str = "Learning Java"; 
+RemoveDuplicates r = new RemoveDuplicates(); 
+r.removeDuplicates(str); 
+} 
+} 
+  
+
